12287fe9c32dff3983acc762910dd0354f1d40f3
zcj-git520 <909536346@qq.com>
2026-06-09 16:44:42 +0800

    feat(core): AuthInterceptor 401 handling with serial refresh lock

--- lib/core/network/interceptors/auth_interceptor.dart
+++ lib/core/network/interceptors/auth_interceptor.dart
@@ -1,13 +1,29 @@
+import 'dart:async';
 import 'package:dio/dio.dart';
 import 'package:hot_ai_app/core/storage/secure_storage.dart';
 
 typedef OnUnauthorized = Future<bool> Function();
 
+class _AuthState {
+  bool refreshing = false;
+  Completer<bool>? inFlight;
+}
+
 class AuthInterceptor extends Interceptor {
-  AuthInterceptor({required this.storage, this.onUnauthorized});
+  AuthInterceptor({required this.storage, this.onUnauthorized, Dio? dio}) : _dio = dio;
 
   final TokenStorage storage;
   final OnUnauthorized? onUnauthorized;
+  final Dio? _dio;
+
+  static final _state = _AuthState();
+  static Completer<bool>? get _inFlight => _state.inFlight;
+  static set _inFlight(Completer<bool>? c) => _state.inFlight = c;
+
+  static void resetState() {
+    _state.refreshing = false;
+    _state.inFlight = null;
+  }
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
@@ -17,5 +33,43 @@ class AuthInterceptor extends Interceptor {
       options.headers['Authorization'] = 'Bearer $token';
     }
     handler.next(options);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) async {
+    if (err.response?.statusCode != 401) return handler.next(err);
+    if (err.requestOptions.extra['_retried'] == true) return handler.next(err);
+    err.requestOptions.extra['_retried'] = true;
+
+    final completer = _inFlight;
+    if (completer != null) {
+      final ok = await completer.future;
+      if (!ok) return handler.next(err);
+      return _retry(err, handler);
+    }
+    final c = Completer<bool>();
+    _inFlight = c;
+    bool ok = false;
+    try {
+      ok = await (onUnauthorized?.call() ?? Future.value(false));
+    } catch (_) {
+      ok = false;
+    }
+    c.complete(ok);
+    _inFlight = null;
+    if (!ok) return handler.next(err);
+    _retry(err, handler);
+  }
+
+  void _retry(DioException err, ErrorInterceptorHandler handler) async {
+    try {
+      final token = await storage.readAccess();
+      err.requestOptions.headers['Authorization'] = token == null ? '' : 'Bearer $token';
+      final dio = _dio ?? Dio();
+      final response = await dio.fetch(err.requestOptions);
+      handler.resolve(response);
+    } catch (e) {
+      if (e is DioException) handler.next(e);
+    }
   }
 }
--- test/core/network/interceptors/auth_interceptor_test.dart
+++ test/core/network/interceptors/auth_interceptor_test.dart
@@ -17,6 +17,42 @@ class _FakeTokenStorage implements TokenStorage {
   Future<void> clear() async => access = null;
 }
 
+class _CapturingAdapter implements HttpClientAdapter {
+  _CapturingAdapter(this.onHeaders);
+  final void Function(Map<String, dynamic>) onHeaders;
+  @override
+  Future<ResponseBody> fetch(RequestOptions options, Stream<List<int>>? s, Future<void>? c) async {
+    onHeaders(options.headers);
+    return ResponseBody.fromString('{"code":0,"data":{},"message":"ok"}', 200, headers: {
+      Headers.contentTypeHeader: ['application/json'],
+    });
+  }
+  @override
+  void close({bool force = false}) {}
+}
+
+class _SeqAdapter implements HttpClientAdapter {
+  _SeqAdapter(this.responses);
+  final List<Response> responses;
+  int _idx = 0;
+  @override
+  Future<ResponseBody> fetch(RequestOptions o, Stream<List<int>>? s, Future<void>? c) async {
+    final r = responses[_idx++];
+    if (r.statusCode == 401) {
+      throw DioException(
+        requestOptions: o,
+        response: r,
+        type: DioExceptionType.badResponse,
+      );
+    }
+    return ResponseBody.fromString('{"code":0,"data":{},"message":"ok"}', 200, headers: {
+      Headers.contentTypeHeader: ['application/json'],
+    });
+  }
+  @override
+  void close({bool force = false}) {}
+}
+
 void main() {
   test('请求自动加 Authorization header', () async {
     final storage = _FakeTokenStorage()..access = 'token-abc';
@@ -35,18 +71,27 @@ void main() {
     await dio.get('/x');
     expect(captured, isNull);
   });
-}
 
-class _CapturingAdapter implements HttpClientAdapter {
-  _CapturingAdapter(this.onHeaders);
-  final void Function(Map<String, dynamic>) onHeaders;
-  @override
-  Future<ResponseBody> fetch(RequestOptions options, Stream<List<int>>? s, Future<void>? c) async {
-    onHeaders(options.headers);
-    return ResponseBody.fromString('{"code":0,"data":{},"message":"ok"}', 200, headers: {
-      Headers.contentTypeHeader: ['application/json'],
-    });
-  }
-  @override
-  void close({bool force = false}) {}
+  test('401 触发 onUnauthorized,成功后重发原请求', () async {
+    AuthInterceptor.resetState();
+    final storage = _FakeTokenStorage()..access = 'old';
+    int callCount = 0;
+    final dio = Dio(BaseOptions(baseUrl: 'https://example.com'))
+      ..httpClientAdapter = _SeqAdapter([
+        Response(requestOptions: RequestOptions(path: '/x'), statusCode: 401),
+        Response(requestOptions: RequestOptions(path: '/x'), statusCode: 200),
+      ]);
+    dio.interceptors.add(AuthInterceptor(
+      dio: dio,
+      storage: storage,
+      onUnauthorized: () async {
+        callCount++;
+        await storage.writeAccess('new');
+        return true;
+      },
+    ));
+    final r = await dio.get('/x');
+    expect(r.statusCode, 200);
+    expect(callCount, 1);
+  });
 }
